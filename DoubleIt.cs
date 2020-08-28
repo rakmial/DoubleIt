7e5598fc718e53bf03cfa43a26b7c65d4e26420a
Joshua Lovelace <rakmial@gmail.com>
2020-08-28 10:06:17 -0700

    doubles input number

--- DoubleIt.cs
+++ DoubleIt.cs
@@ -5,6 +5,10 @@ class DoubleIt
   static void Main()
   {
     Console.WriteLine("What number should I double?");
-    int yourNumber = Console.ReadLine();
+    string inputString = Console.ReadLine();
+    int yourNumber = int.Parse(inputString);
+    int yourDoubledNumber = yourNumber * 2;
+    string stringDouble = yourDoubledNumber.ToString();
+    Console.WriteLine(stringDouble + " is double " + inputString);
   }
 }
